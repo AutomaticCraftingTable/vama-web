9dcce72fb11f114f7176779f6ebf68e18ebbe61a
addyyio <adx1213@gmail.com>
2025-03-22 13:07:03 +0100

    article

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -7,13 +7,13 @@ import { ref } from 'vue'
   <div class="Card flex items-center border border-gray-300 rounded-md max-w-sm p-6 gap-5">
     <div class="AuthorInfo flex gap-2">
       <a href="Profile.vue"><img src="/vite.svg"/></a>
-      <div class="NickFollows flex-col font-bold">
+      <div class="NickFollows font-bold">
         <p>Nickname</p>
         <p class="text-gray-500">69 obserwujących</p>
       </div>
     </div>
     <div>
-      <p class="text-gray-500">Naprawiam UI duolin go </p>
+      <p class="text-gray-500">Naprawiam UI duolingo</p>
     </div>
   </div>
 </template>
