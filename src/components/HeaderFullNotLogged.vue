6da9872cb8115e83149375e138e5be0fdfcc043d
addyyio <adx1213@gmail.com>
2025-04-13 13:58:46 +0200

    New pages

---
+++ src/components/HeaderFullNotLogged.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{
+  msg?: string
+}>()
+
+const isLoggedIn = ref(false)
+const searchQuery = ref('')
+</script>
+
+<template>
+  <div class="w-full flex flex-col bg-background">
+    <header class="w-full py-2.5">
+      <div class="flex items-center justify-between w-full px-5">
+        <div class="flex items-center gap-2 text-2xl">
+          <a href="/homeNotLogged">
+            <div class="w-10 h-10 flex items-center justify-center font-bold text-white-text bg-primary-main-light">V</div>
+          </a>
+          <div>Vama</div>
+        </div>
+
+        <div class="flex-grow mx-5">
+          <div class="flex items-center bg-secondary-main-light rounded-sm px-4 py-2 text-secondary-text">
+            <span class="mr-2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+                   stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <circle cx="11" cy="11" r="8"></circle>
+                <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+              </svg>
+            </span>
+            <input
+                type="text"
+                placeholder="Szukaj..."
+                v-model="searchQuery"
+                class="border-none bg-transparent w-full outline-none text-base  transition duration-150 ease-in-out"
+            />
+          </div>
+        </div>
+
+        <div class="flex gap-2.5">
+          <router-link to="/login"><button class="px-5 py-2 rounded-sm border-none cursor-pointer font-medium bg-secondary-main-light hover:bg-secondary-hover-light text-black-text">Zaloguj się</button></router-link>
+          <router-link to="/Registration"><button class="px-5 py-2 rounded-sm border-none cursor-pointer font-medium bg-primary-main-light text-white-text hover:bg-primary-hover-light">Zajerestuj się</button></router-link>
+          <router-view />
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
