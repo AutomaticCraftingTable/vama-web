dbcc7f29edef2c6dcf95d47811d99a8876b8b79e
addyyio <adx1213@gmail.com>
2025-05-09 17:11:29 +0200

    Błąd weryfikacji

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/pages/Login.vue'
 import Registration from '@/pages/Registration.vue'
 import Home from '@/pages/Home.vue'
+import VeriFailed from '@/pages/VeriFailed.vue'
 
 const routes = [
   { path: '/login', component: Login },
   { path: '/registration', component: Registration },
   { path: '/', component: Home },
+  { path: '/emailFailed', component: VeriFailed },
 ]
 
 const router = createRouter({
