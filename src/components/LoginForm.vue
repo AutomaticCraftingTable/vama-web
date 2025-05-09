2bb3727439ef7f9397f0ff8646fe5d9f773b8a0b
addyyio <adx1213@gmail.com>
2025-05-09 15:54:27 +0200

    Poprawiony wygląd stron

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -14,35 +14,33 @@ const handleLogin = () => {
 }
 </script>
 <template>
-  <div class="flex flex-col min-h-screen">
+  <div class="flex flex-col min-h-screen bg-bg">
     <div class="flex flex-1">
       <div class="hidden md:block w-1/2 bg-cover bg-center"><img src="/LoginBG.png"></div>
       <div class="w-full md:w-1/2 flex justify-center items-center p-8">
         <div class="w-full max-w-md">
           <h1 class="text-2xl font-semibold text-text m-0 mb-2">Witamy w VAMA</h1>
-          <p class="text-base text-secondary-text m-0 mb-6">Podziel się swoją historią</p>
+          <p class="text-base text-text-secondary m-0 mb-6">Podziel się swoją historią</p>
           <form @submit.prevent="handleLogin" class="w-full flex flex-col gap-6">
-            <div class="flex flex-col gap-2">
-              <label for="email" class="text-base font-medium">Email</label>
+            <div class="flex flex-col gap-2 bg-secondary">
               <input
                 id="email"
                 type="email"
                 v-model="email"
                 placeholder="Email"
                 required
-                class="w-full py-3.5 px-4 border rounded-sm text-base outline-none transition-colors border-secondary focus:border-secondary-hover"
+                class="w-full py-3.5 px-4 rounded-sm text-text-secondary outline-none transition-colors"
               />
             </div>
-            <div class="flex flex-col gap-2">
-              <label for="password" class="text-base font-medium">Hasło</label>
-              <div class="mb-4 relative">
+            <div class="flex flex-col gap-2 ">
+              <div class="mb-4 relative bg-secondary">
                 <input
                   id="password"
                   :type="showPassword ? 'text' : 'password'"
                   v-model="password"
                   placeholder="Hasło"
                   required
-                  class="w-full py-3.5 px-4 border  rounded-sm text-base outline-none transition-colors border-secondary focus:border-secondary-hover"
+                  class="w-full py-3.5 px-4 rounded-sm text-text-secondary outline-none transition-colors"
                 />
                 <button
                   type="button"
@@ -56,7 +54,7 @@ const handleLogin = () => {
                 </button>
               </div>
             </div>
-            <button type="submit" class="w-full py-3.5 px-4 rounded-sm font-semibold text-base transition-colors text-white-text bg-primary hover:bg-primary-hover">Zaloguj się</button>
+            <button type="submit" class="w-full py-3.5 px-4 rounded-sm font-semibold text-base transition-colors text-text-primary bg-primary hover:bg-primary-hover">Zaloguj się</button>
           </form>
         </div>
       </div>
--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -19,40 +19,44 @@ const handleRegister = () => {
 }
 </script>
 <template>
-  <div class="flex justify-center items-center min-h-screen bg-background p-5">
+  <div class="flex justify-center items-center min-h-screen bg-bg p-5">
     <div class="w-full max-w-md bg-background rounded-lg p-8 shadow-xl">
-      <h1 class="text-2xl font-semibold text-dark-text m-0 mb-2">Witamy w VAMA</h1>
+      <h1 class="text-2xl font-semibold text-text m-0 mb-2">Witamy w VAMA</h1>
       <p class="text-base text-text-secondary m-0 mb-6">Rozpocznij pisanie swoich historii</p>
 
-      <form @submit.prevent="handleRegister">
-        <div class="mb-4">
+      <form @submit.prevent="handleRegister" class="w-full flex flex-col gap-6">
+        <div class="flex flex-col gap-2 bg-secondary">
           <input
+              id="email"
               type="email"
               v-model="email"
               placeholder="Email"
               required
-              class="w-full py-3.5 px-4 border border-background-secondary rounded-md text-base outline-none transition-colors focus:border-primary"
+              class="w-full py-3.5 px-4 rounded-sm text-text-secondary outline-none transition-colors"
           />
         </div>
 
-        <div class="mb-4 relative">
-          <input
-              :type="showPassword ? 'text' : 'password'"
-              v-model="password"
-              placeholder="Hasło"
-              required
-              class="w-full py-3.5 px-4 border border-background-secondary rounded-md text-base outline-none transition-colors focus:border-primary"
-          />
-          <button
-              type="button"
-              class="absolute right-3 top-1/2 transform -translate-y-1/2 bg-transparent border-none cursor-pointer text-text-secondary flex items-center justify-center p-0"
-              @click="togglePasswordVisibility"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7z"></path>
-              <circle cx="12" cy="12" r="3"></circle>
-            </svg>
-          </button>
+        <div class="flex flex-col gap-2 ">
+          <div class="mb-4 relative bg-secondary">
+            <input
+                id="password"
+                :type="showPassword ? 'text' : 'password'"
+                v-model="password"
+                placeholder="Hasło"
+                required
+                class="w-full py-3.5 px-4 rounded-sm text-text-secondary outline-none transition-colors"
+            />
+            <button
+                type="button"
+                class="absolute right-3 top-1/2 transform -translate-y-1/2 bg-transparent border-none cursor-pointer text-secondary-text flex items-center justify-center p-0"
+                @click="togglePasswordVisibility"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7z"></path>
+                <circle cx="12" cy="12" r="3"></circle>
+              </svg>
+            </button>
+          </div>
         </div>
 
         <div class="my-4">
@@ -69,7 +73,7 @@ const handleRegister = () => {
 
         <button
             type="submit"
-            class="w-full py-3.5 px-4 bg-primary text-text font-bold border-none rounded-md text-base cursor-pointer mt-2 transition-colors hover:bg-accent"
+            class="w-full py-3.5 px-4 bg-primary hover:bg-primary-hover text-text-primary font-bold border-none rounded-md text-base cursor-pointer mt-2 transition-colors"
         >
           Zarejestruj się
         </button>
