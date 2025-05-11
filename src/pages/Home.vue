4c1c3fe71be00660d3a293ee057635bd03518a54
addyyio <adx1213@gmail.com>
2025-05-11 14:11:14 +0200

    Fix

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,31 @@
 <script setup lang="ts">
-
+import { ref, onMounted } from 'vue'
 import Header from '@/components/Header.vue'
 import Article from '@/components/Article.vue'
 import SideBar from '@/components/SideBarHome.vue'
+import axios from 'axios'
 
+const articles = ref([])
+const role= ref('guest')
 
-defineProps<{ msg?: string }>()
+onMounted(() => {
+  const url = 'http://127.0.0.1:3658/m1/829899-809617-default/api/home'
+  axios.get(url).then(response => {
+    articles.value = response.data.articles
+    role.value = response.data.role
+  }).catch(error => {
+    console.error('Błąd podczas pobierania danych:', error)
+  })
+
+})
 </script>
 
 <template>
   <Header></Header>
-  <router-view />
   <div class="flex h-screen">
-      <SideBar/>
-      <Article />
+    <SideBar v-if="role !== 'guest'" />
+    <div class="flex flex-row gap-7 mt-7">
+      <Article v-for="(article, index) in articles" :key="index" :article="article" />
+    </div>
   </div>
 </template>
