c4bec10130742f8a0d087efc95eaf9826704defa
addyyio <adx1213@gmail.com>
2025-05-11 16:33:58 +0200

    Fix

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,28 +1,30 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import HeaderFullNotLogged from '@/components/HeaderFullNotLogged.vue'
-import HeaderFullLogged from '@/components/HeaderFullLogged.vue'
+import Header from '@/components/Header.vue'
 import Article from '@/components/Article.vue'
 import SideBar from '@/components/SideBarHome.vue'
+import axios from 'axios'
 
-const isLoggedIn = ref(true)
+const articles = ref([])
+const role= ref('guest')
 
 onMounted(() => {
   const url = 'http://127.0.0.1:3658/m1/829899-809617-default/api/home'
   axios.get(url).then(response => {
     articles.value = response.data.articles
+    role.value = response.data.role
   }).catch(error => {
-    console.error('Błąd podczas pobierania artykułów:', error)
+    console.error('Błąd podczas pobierania danych:', error)
   })
+
 })
 </script>
 
 <template>
-  <component :is="isLoggedIn ? HeaderFullLogged : HeaderFullNotLogged" />
-
+  <Header></Header>
   <div class="flex h-screen">
-    <SideBar v-if="isLoggedIn" />
-    <div :class="['flex flex-row gap-7 mt-7', isLoggedIn ? 'ml-7' : 'ml-7']">
+    <SideBar v-if="role !== 'guest'" />
+    <div class="flex flex-row gap-7 mt-7">
       <Article v-for="(article, index) in articles" :key="index" :article="article" />
     </div>
   </div>
