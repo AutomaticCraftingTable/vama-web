c8f9165043eae5507b79eb2471f72a193843b932
addyyio <adx1213@gmail.com>
2025-06-04 10:37:17 +0200

    Fix styli plików ikon

--- src/components/Icons/Eye.vue
+++ src/components/Icons/Eye.vue
@@ -1,6 +1,3 @@
-<script>
-
-</script>
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" class="fill-none stroke-text size-6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
     <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7z"></path>
