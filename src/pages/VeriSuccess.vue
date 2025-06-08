5ae85596bfc2f215e919e8246e23a550e97686dc
AdrianMatys <146954279+AdrianMatys@users.noreply.github.com>
2025-06-08 15:19:05 +0200

    Veri email notyfications (#8)
    * New styles and themes
    
    * New pages
    
    * Fix SideBar
    
    * Fix SideBar
    
    * New App.vue
    
    * Background in login page
    
    * Połączenie strony Home, Dodanie motywów strony, Zmiana logo
    
    * Poprawiony Article, Login, SideBar na Home
    
    * Poprawiony Header
    
    * Poprawione Article
    
    * Fix
    
    * Błąd weryfikacji
    
    * Poprawna weryfikacja oraz dodanie tła do błędnej weryfikacji
    
    * Dodanie strony informującej o konieczności weryfikacji

---
+++ src/pages/VeriSuccess.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HeaderFullNotLogged from '@/components/HeaderFullNotLogged.vue'
+import VeriEmailSuccess from '@/components/VeriEmailSuccess.vue'
+</script>
+
+<template>
+  <HeaderFullNotLogged></HeaderFullNotLogged>
+  <VeriEmailSuccess></VeriEmailSuccess>
+</template>
