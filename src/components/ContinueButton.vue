645e324fd04969fc15e197640065987f1918f7bc
addyyio <adx1213@gmail.com>
2025-06-13 00:01:24 +0200

    GitHub-pages

--- src/components/ContinueButton.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <button class="w-full py-3 bg-black text-white font-bold rounded-md"
-          aria-label="Kontynuuj"
-          tabindex="0">
-    Kontynuuj
-  </button>
-</template>
--- src/components/Icons/Moon.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <svg xmlns="http://www.w3.org/2000/svg" class="fill-none stroke-text size-6 h-5 w-5" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12.79A9 9 0 1111.21 3a7 7 0 109.79 9.79z" /></svg>
-</template>
